bbcd5931a5276315b1b8ae403ed3540c0e2141bc
aboveStars <93012417+aboveStars@users.noreply.github.com>
2023-09-04 22:54:10 +0300

    Static background has been added for mobile devices.

--- src/layout/SmallScreenLayout.tsx
+++ src/layout/SmallScreenLayout.tsx
@@ -4,12 +4,13 @@ import ProviderSection from "@/components/sections/ProviderSection";
 import SmallPhoneSection from "@/components/sections/SmallPhoneSection";
 import UserSection from "@/components/sections/UserSection";
 import WelcomeSection from "@/components/sections/WelcomeSection";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 
 export default function SmallScreenLayout() {
   return (
     <Flex id="small-screen" direction="column">
+      <Image src="/bgForMobile.png" position="fixed" objectFit="fill" />
       <SmallPhoneSection />
       <WelcomeSection />
       <UserSection />
