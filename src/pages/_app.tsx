d292ce9f56ef30a0599b755af0cf09121d38f210
aboveStars <93012417+aboveStars@users.noreply.github.com>
2023-09-08 02:56:38 +0300

    Some improvements have been made.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,30 @@
 import { theme } from "@/theme/theme";
 import { ChakraBaseProvider } from "@chakra-ui/react";
 import { Analytics } from "@vercel/analytics/react";
+import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+
+      <DefaultSeo
+        title="Apidon • Next-G Social Media"
+        description="The magnificent platform enables users to select their algorithms, create NFTs that are marketable, and additionally welcomes algorithm creators to participate and generate income."
+        openGraph={{
+          type: "website",
+          images: [
+            {
+              url: "https://firebasestorage.googleapis.com/v0/b/apidon-info.appspot.com/o/og-image-opt.png?alt=media&token=7c777d81-27c5-4670-8a8a-660eefcfb175",
+            },
+          ],
+        }}
+      />
       <Analytics />
       <RecoilRoot>
         <ChakraBaseProvider theme={theme}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,30 +4,6 @@ import Layout from "../layout/Layout";
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Apidon • Next-G Social Media</title>
-        <link
-          rel="icon"
-          href="https://firebasestorage.googleapis.com/v0/b/apidon-info.appspot.com/o/icon.png?alt=media&token=6dcecf16-7edd-46d5-809a-2b154158c7c8"
-        />
-        <meta
-          name="description"
-          content="The magnificent platform enables users to select their algorithms, create NFTs that are marketable, and additionally welcomes algorithm creators to participate and generate income."
-        />
-        <meta property="og:title" content="Apidon • Next-G Social Media" />
-        <meta
-          property="og:description"
-          content="The magnificent platform enables users to select their algorithms, create NFTs that are marketable, and additionally welcomes algorithm creators to participate and generate income."
-        />
-        <meta
-          property="og:image"
-          content="https://firebasestorage.googleapis.com/v0/b/apidon-info.appspot.com/o/og-image-opt.png?alt=media&token=7c777d81-27c5-4670-8a8a-660eefcfb175"
-        />
-        <meta
-          name="twitter:card"
-          content="https://firebasestorage.googleapis.com/v0/b/apidon-info.appspot.com/o/og-image-opt.png?alt=media&token=7c777d81-27c5-4670-8a8a-660eefcfb175"
-        />
-      </Head>
       <Layout />
     </>
   );
