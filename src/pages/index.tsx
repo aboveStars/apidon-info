86945cd56caa1ef96bc9f7a4a2883ae3a277b448
aboveStars <93012417+aboveStars@users.noreply.github.com>
2023-09-07 14:24:42 +0300

    Some improvements have been made.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
+import Head from "next/head";
 import Layout from "../layout/Layout";
 
 export default function Home() {
-  return <Layout />;
+  return (
+    <>
+      <Head>
+        <title>Apidon • Next-G Social Media</title>
+        <meta
+          name="description"
+          content="The magnificent platform enables users to select their algorithms, create NFTs that are marketable, and additionally welcomes algorithm creators to participate and generate income."
+        />
+        <meta property="og:title" content="Apidon • Next-G Social Media" />
+        <meta
+          property="og:description"
+          content="The magnificent platform enables users to select their algorithms, create NFTs that are marketable, and additionally welcomes algorithm creators to participate and generate income."
+        />
+        <meta
+          property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/apidon-info.appspot.com/o/og-image-opt.png?alt=media&token=7c777d81-27c5-4670-8a8a-660eefcfb175"
+        />
+        <meta
+          name="twitter:card"
+          content="https://firebasestorage.googleapis.com/v0/b/apidon-info.appspot.com/o/og-image-opt.png?alt=media&token=7c777d81-27c5-4670-8a8a-660eefcfb175"
+        />
+      </Head>
+      <Layout />
+    </>
+  );
 }
