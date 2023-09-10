019b7855648817cc30dd61862d0220fe5bb198df
aboveStars <93012417+aboveStars@users.noreply.github.com>
2023-09-10 14:22:19 +0300

    Revert "Some improvements have been made."
    This reverts commit efeee25ef2c8468a11c8e57e4b5b735772d96982.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,33 @@ import { ChakraBaseProvider } from "@chakra-ui/react";
 import { Analytics } from "@vercel/analytics/react";
 import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+
       <DefaultSeo
         title="Apidon • Next-G Social Media"
         description="The magnificent platform enables users to select their algorithms, create NFTs that are marketable, and additionally welcomes algorithm creators to participate and generate income."
